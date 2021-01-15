9135d52a4554c6a6e2ad0da7f8c611fb7d4a3203
Daisugi <volble124@gmail.com>
2021-01-15 02:21:07 -0800

    add assignment7

--- Unit5/assignment7/NavBarAnimation/NavBarAnimation/ViewController.swift
+++ Unit5/assignment7/NavBarAnimation/NavBarAnimation/ViewController.swift
@@ -58,7 +58,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.register(SnackTableViewCell.self, forCellReuseIdentifier: cellId)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
         tableView.dataSource = self
         tableView.delegate = self
         
@@ -71,7 +71,7 @@ class ViewController: UIViewController {
         setupUI()
     }
 
-    func setupUI() {
+    private func setupUI() {
         // set top view constraints
         topView.heightAnchor.constraint(equalToConstant: 88).isActive = true
         topView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0).isActive = true
@@ -102,25 +102,25 @@ class ViewController: UIViewController {
         imageStackView.isHidden = true
 
         // set table view constraints
-        tableView.topAnchor.constraint(equalTo: imageStackView.bottomAnchor, constant: 0).isActive = true
+        tableView.topAnchor.constraint(equalTo: topView.bottomAnchor, constant: 0).isActive = true
         tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0).isActive = true
         tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0).isActive = true
         tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
     }
 
-    @objc func addPressed(_ sender: UIButton) {
+    @objc private func addPressed(_ sender: UIButton) {
         scaleNavBarHeight()
         imageStackView.isHidden = isNavbarHideen
     }
 
-    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer){
+    @objc private func imageTapped(tapGestureRecognizer: UITapGestureRecognizer){
         let tappedImage = tapGestureRecognizer.view as! UIImageView
         guard let name = tappedImage.restorationIdentifier else { return }
         snacks.append(name)
         tableView.insertRows(at: [IndexPath(row: snacks.count - 1, section: 0)], with: .automatic)
     }
 
-    func createImageView(imageName: String) -> UIImageView {
+    private func createImageView(imageName: String) -> UIImageView {
         let iv = UIImageView(image: UIImage(named: imageName))
         iv.translatesAutoresizingMaskIntoConstraints = false
         iv.contentMode = .scaleToFill
@@ -131,20 +131,20 @@ class ViewController: UIViewController {
         return iv
     }
 
-    func scaleNavBarHeight() {
+    private func scaleNavBarHeight() {
         UIView.animate(withDuration: 1.0, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.3, animations: {
             if self.isNavbarHideen {
                 let scaleTopView = CGAffineTransform(scaleX: 1.0, y: 3.3)
                 self.topView.transform = scaleTopView
 
-                let translateTitle = CGAffineTransform(translationX: self.topView.frame.width / 2 - 200, y: self.topView.bounds.size.height / 2 - 30)
+                let translateTitle = CGAffineTransform(translationX: self.topView.frame.width / 2 - 190, y: self.topView.bounds.size.height / 2 - 30)
                 self.navTitle.text = "ADD a Snack"
                 self.navTitle.transform = translateTitle
                 
                 let rotateAddButton = CGAffineTransform(rotationAngle: .pi / 4)
                 self.addButton.transform = rotateAddButton
 
-                self.tableView.contentInset = UIEdgeInsets(top: self.topView.bounds.height + 10, left: 0, bottom: 0, right: 0)
+                self.tableView.contentInset = UIEdgeInsets(top: self.topView.bounds.height + 15, left: 0, bottom: 0, right: 0)
             } else {
                 self.topView.transform = .identity
                 self.navTitle.transform = .identity
@@ -152,24 +152,22 @@ class ViewController: UIViewController {
                 self.addButton.transform = .identity
                 self.tableView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
             }
-
             self.isNavbarHideen.toggle()
         }, completion: nil)
     }
 }
 
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return snacks.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! SnackTableViewCell        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
         cell.textLabel?.text = snacks[indexPath.row]
         return cell
-    }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
     }
 }
